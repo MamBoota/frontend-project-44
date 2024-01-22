086596db77a6b10dbe56124b3aa0c503535ced45
MamBoota <deniskostyuk.93@yandex.ru>
2024-01-23 01:22:51 +0300

    Lvl 4, completed

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+import getReadLineSync from "../src/cli.js";
 
-const greeting = 'Welcome to the Brain Games!';
+const greeting = 'Welcome to the Brain Games!\nMay I have your name?';
 console.log(greeting);
+console.log(`Hello, ${getReadLineSync()}!`);
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readLineSync from 'readline-sync';
+
+const getReadLineSync = () => {
+    const question = readLineSync.question('Your answer: ');
+    return question;
+};
+
+export default getReadLineSync;
